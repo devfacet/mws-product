8824ed7537e777a960834975e3c82ee8ac0e9998
cmfatih <cmuratfatih@gmail.com>
2014-04-26 04:31:01 -0400

    Add unit test

--- test/test-all.js
+++ test/test-all.js
@@ -1,53 +1,72 @@
 // Init reqs
 /* jslint node: true */
+/* global describe: false */
+/* global it: false */
 'use strict';
 
-var mwsprod = require('../'),
-    utilex  = require('utilex')
+var mwsProd = require('../'),
+    utilex  = require('utilex'),
+    expect  = require('chai').expect
 ;
-
-// Init vars
-var testList  = {SERVICESTATUS: true, MATCHINGPRODUCTFORID: false},
-    auth      = {sellerId: 'SELLERID', accessKeyId: 'ACCESSKEYID', secretKey: 'SECRETKEY'},
-    mplace    = 'US',
-    mwsProd
-;
-
-if(utilex.tidyArgs().testDEV !== undefined) {
-  testList.SERVICESTATUS              = true;
-  testList.MATCHINGPRODUCTFORID       = true;
-  if(utilex.tidyArgs().authJSON) auth = require(utilex.tidyArgs().authJSON);
-}
-
-mwsProd = mwsprod({auth: auth, marketplace: mplace});
 
 // Tests
-utilex.tidyLog('test-all.js');
 
-// Test for service status
-if(testList.SERVICESTATUS === true) {
-  mwsProd.serviceStatus(function(err, data) {
-    utilex.tidyLog('SERVICESTATUS:');
+// Test for amazon seller module
+describe('mwsProd', function() {
 
-    if(!err) {
-      console.log(JSON.stringify(data, null, 2));
-    }
-    else {
-      console.log(JSON.stringify(err, null, 2));
-    }
+  // Init vars
+  var auth        = {sellerId: 'SELLERID', accessKeyId: 'ACCESSKEYID', secretKey: 'SECRETKEY'},
+      mplace      = 'US',
+      appMWSProd  // mwsProd instance
+  ;
+
+  if(utilex.tidyArgs().testDEV !== undefined && utilex.tidyArgs().authJSON) {
+    auth = require(utilex.tidyArgs().authJSON);
+  }
+
+  appMWSProd = mwsProd({auth: auth, marketplace: mplace});
+
+  it('should get service status ', function(done) {
+    appMWSProd.serviceStatus(function(err, data) {
+      if(err) {
+        done(err.message);
+        return;
+      }
+
+      expect(data).to.have.property('GetServiceStatusResponse');
+      var gssr = data.GetServiceStatusResponse;
+      expect(gssr).to.be.a('object');
+      expect(gssr).to.have.property('GetServiceStatusResult');
+      expect(gssr.GetServiceStatusResult).to.be.a('object');
+      expect(gssr.GetServiceStatusResult).to.have.property('Status');
+      expect(gssr.GetServiceStatusResult.Status).to.be.a('string');
+      done();
+    });
   });
-}
 
-// Test for matching product for id
-if(testList.MATCHINGPRODUCTFORID === true) {
-  mwsProd.matchingProductForId({idType: 'ASIN', idList: ['B00863WC40', 'B008648946']}, function(err, data) {
-    utilex.tidyLog('MATCHINGPRODUCTFORID:');
+  if(auth.sellerId && auth.sellerId != 'SELLERID') {
 
-    if(!err) {
-      console.log(JSON.stringify(data, null, 2));
-    }
-    else {
-      console.log(JSON.stringify(err, null, 2));
-    }
-  });
-}
+    // Init var
+    var prodID = 'B00863WC40';
+
+    it('should get matching products for ' + prodID, function(done) {
+      appMWSProd.matchingProductForId({idType: 'ASIN', idList: [prodID]}, function(err, data) {
+        if(err) {
+          done(err.message);
+          return;
+        }
+
+        expect(data).to.have.property('GetMatchingProductForIdResponse');
+        var gmpfiresp = data.GetMatchingProductForIdResponse;
+        expect(gmpfiresp).to.be.a('object');
+        expect(gmpfiresp).to.have.property('GetMatchingProductForIdResult');
+        var gmpfires = gmpfiresp.GetMatchingProductForIdResult;
+        expect(gmpfires).to.be.a('array');
+        expect(gmpfires[0]).to.have.property('A$');
+        expect(gmpfires[0]['A$']).to.be.a('object');
+        expect(gmpfires[0]['A$']).to.have.property('Id', prodID);
+        done();
+      });
+    });
+  }
+});
